3ab01fe6c7f640903c8f5083fc4779bd46839842
Bernal Thiago <thiagoa.bernal@yahoo.com.ar>
2023-09-12 17:33:28 -0300

    fix de persistencia en marca y tipo vehiculo

--- src/main/java/com/concesionaria/concesionaria/repositorios/TipoVehiculoRepositorio.java
+++ src/main/java/com/concesionaria/concesionaria/repositorios/TipoVehiculoRepositorio.java
@@ -1,10 +1,15 @@
 package com.concesionaria.concesionaria.repositorios;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.concesionaria.concesionaria.entidades.*;
 
 @Repository
 public interface TipoVehiculoRepositorio extends CrudRepository<TipoVehiculo, Long>{
-    
+
+    @Query("SELECT CASE WHEN COUNT(v) > 0 THEN true ELSE false END FROM Vehiculo v WHERE v.tipoVehiculo.id = ?1")
+    boolean hasReferences(Long id);
+
+
 }
